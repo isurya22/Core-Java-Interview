c488881caf5bfdad8e9a61230f31679a713af301
surya <suryanarayan939@gmail.com>
2024-01-18 23:54:21 +0530

    Java8 Interface Features

---
+++ src/InterviewAsked/ReverseString.java
@@ -0,0 +1,16 @@
+package InterviewAsked;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String word = "Surya";
+
+        ReverseString.reverseProcess(word);
+    }
+    public static void reverseProcess(String w){
+        String reverseWord = "";
+     for(int i = w.length()-1; i>=0 ; i--){
+         reverseWord += w.charAt(i);
+     }
+        System.out.println("ReverseWord :"+reverseWord);
+    }
+}
